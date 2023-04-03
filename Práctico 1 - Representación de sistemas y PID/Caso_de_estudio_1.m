1d2e6d331a1bf2b14a9bdfda248b62364d1e3b1f
Facundo Galvagno <facundo.galvagno@outlook.com>
2023-04-02 22:05:57 -0300

    determinada la funcion de transferencia de los datos del xlsx

--- Práctico 1 - Representación de sistemas y PID/Caso_de_estudio_1.m
+++ Práctico 1 - Representación de sistemas y PID/Caso_de_estudio_1.m
@@ -1,6 +1,7 @@
 clc;clear all;close all;
 % Generamos señal de entrada:
 % Onda cuadrada alterna de 2ms de periodo y 12V de amplitud
+%{
 [u,t] = gensig("square",2e-03,4e-03);
 u=24*u-12;
 R=4.7e03; L=10e-06; C=100e-09; %Parámetros del circuito RLC
@@ -38,15 +39,50 @@ figure('Name', '1.2')
  subplot(3,1,1);plot(t,i);grid on; title('Corriente1');
  subplot(3,1,2);plot(t,vc);grid on; title('Tensión en el capacitor, V_c1');
  subplot(3,1,3);plot(t,u);grid on; title('Tensión de entrada, V_e1');
-
+%}
 %Cargamos los datos medidos
-data = readtable('Curvas_Medidas_RLC.xls')
-data = renamevars(data, ["Var1","Var2","Var3"], ["t","i","v_c"])
 
-figure('Name', '1.2')
- subplot(3,1,1);plot(data.t,data.i);grid on; title('Corriente1');
- subplot(3,1,2);plot(data.t,data.v_c);grid on; title('Tensión en el capacitor, V_c1');
 
-%Determinacion del modelo a partir de la ecuacion de 2do orden
+datos = table2array(readtable('Curvas_Medidas_RLC.xls'));
+t = datos(:,1);
+v_c = datos(:,3);
 
+
+
+%figure('Name', '1.2')
+ subplot(3,1,1);plot(datos(:,1),datos(:,2));grid on; title('Corriente1');
+ subplot(3,1,2);plot(datos(:,1),datos(:,3));grid on; title('Tensión en el capacitor, V_c1');
+
+% Determinamos el modelo del circuito RLC a partir del metodo desarollado
+% por Chen
+
+td = 0.01
+t1 = 0.002
+
+k=max(v_c)
+
+[val lugar] = min(abs((t1+td)-t));
+k1 = v_c(lugar)/k-1
+[val lugar] = min(abs((2*t1+td)-t));
+k2 = v_c(lugar)/k-1
+[val lugar] = min(abs((3*t1+td)-t));
+k3 = v_c(lugar)/k-1
+
+b=4*k1^3*k3-3*k1^2*k2^2-4*k2^3+k3^2+6*k1*k2*k3;
+alfa1 = (k1*k2+k3-sqrt(b))/(2*(k1^2+k2))
+alfa2 = (k1*k2+k3+sqrt(b))/(2*(k1^2+k2))
+beta= (2*k1^3+3*k1*k2+k3-sqrt(b))/sqrt(b)
+
+T1 =-t1/log(alfa1)
+T2 =-t1/log(alfa2)
+%T3 = beta*(T1-T2)+T1
+
+
+s = tf('s')
+
+%sys_id = k*(T3*s+1)/(T1*s+1)/(T2*s+1)
+sys_id = k*(T1*s+1)/(T2*s+1)
+%V_c/V = 1/(LCs^2+R1*C*s+1)
+
+step(sys_id)
 
