5f95c20991a438d22cdac2f57a2b8f5883ed1ef2
Facundo Galvagno <galvagno.facundo@outlook.com>
2024-06-10 13:34:58 -0300

    item 3

--- Práctico 2 - Diseño de controladores en variables de estado en tiempo continuo/mot1_lqr_obs.m
+++ Práctico 2 - Diseño de controladores en variables de estado en tiempo continuo/mot1_lqr_obs.m
@@ -8,15 +8,15 @@ Km=0.0605;
 %% Modelo del motor en espacio de estados
 
 A = [-Ra/Laa -Km/Laa 0; Ki/J -Bm/J 0; 0 1 0];
-B = [1/Laa 0; 0 -1/J; 0 0];
-C = [1 0 0; 0 1 0; 0 0 1];
+B = [1/Laa; 0; 0];
+C = [0 1 0; 0 0 1];
 D = [0 0; 0 0; 0 0];
 
 Q = diag([62500 2.5e-5  1  10])
 R = 1;
 
-% Matriz ampliada (referencia diferente de 0)
-Aa = [A zeros(3,1); -C(3,:) 0]
+% Matriz ampliada 
+Aa = [A zeros(3,1); -C(2,:) 0]
 Ba = [B(:,1); 0]
 
 %Q=diag([100 50 10 ]);R=1e-1;
@@ -35,22 +35,24 @@ MX1=MX1X2(1:n/2,:); MX2=MX1X2(n/2+1:end,:);
  % P=abs(MX2*inv(MX1));
 P=real(MX2*inv(MX1));
 K=inv(R)*Ba'*P;
-
+disp('Controlador ampliado en ')
+eig(Aa-Ba*K)
 %% Calculo del observador
 Ao = A';
-Bo = C(2:3,:)';
+Bo = C';
 Co = B';
-Qo = diag([1 10 100]);
-Ro = diag([1 1]);
-Ko = lqr(Ao,Bo,Qo,Ro);
-Ko = Ko';
-poles_o=eig(A-Ko*C(2:3,:))
+Qo = diag([1e1 10 .09]);
+Ro = diag([1e1 1e1]);
+Ko = lqr(Ao,Bo,Qo,Ro)';
+%poles_o=eig(A-Ko*C(2:3,:))
+disp('Observador en ')
+eig(A-Ko*C)
 %% Calculo del tiempo de simulacion
 poles = eig(Aa-Ba(:,1)*K)
 lambda = max(poles);
 tr = abs(log(0.95)/lambda);
 delta = 2.5e-7;
-ts = 3;
+ts = 0.5;
 
 %% Señales de entrada
 t_s =0:delta:(ts-delta);
@@ -60,6 +62,11 @@ u = zeros(1,ts/delta);
 wr = zeros(1,ts/delta);
 o_ref = zeros(1,ts/delta);
 o = zeros(1,ts/delta);
+
+ia_o = zeros(1,ts/delta);
+wr_o = zeros(1,ts/delta);
+o_o = zeros(1,ts/delta);
+e_o = zeros(2,ts/delta);
 va = zeros(1,ts/delta);
 ia(1)=0; wr(1)=0; o(1)=0; %Cond. iniciales
 X_hat = [ia(1) wr(1) o(1)]';
@@ -86,71 +93,77 @@ o(1) = 0;
 wr(1) = 0;
 %T_L =  1.03e-3;
 %T_L =  0;
-x = [ia(1); wr(1); o(1)];
-X = [ia(1);wr(1);o(1)];
-u(2,:) = 0;
-y(2,:) = 0;
-Y_o(2,:) = 0;
+X = [ia(1); wr(1); o(1)];
+u(1) = 0;
 index = 0;
 int = 0;
+zeta = 0;
 for i = 1:(ts/delta-1)
-      y(:,i) = C(2:3,:)*X;
-      psi_p = o_ref(i)-C(3,:)*X;
-      psi(i) = int + psi_p*delta;
-      u(i) = -K(1:3)*X_hat-K(4)*psi(i);
-      ia(i) = x(1);
-      wr(i) = x(2);
-      o(i) = x(3);
-      x1_p = -Ra*x(1)/Laa-Km*x(2)/Laa+u(i)/Laa;
-      x2_p = Ki*x(1)/J-Bm*x(2)/J-T_L/J;
-      x3_p = x(2);
+      zeta_p = o_ref(i)-C(2,:)*X;
+      zeta = zeta + zeta_p*delta;
+      u(i) = -K*[X;zeta];
+      x1_p = -Ra*X(1)/Laa-Km*X(2)/Laa+u(i)/Laa;
+      x2_p = Ki*X(1)/J-Bm*X(2)/J-T_L/J;
+      x3_p = X(2);
       xP = [x1_p x2_p x3_p]';
-      x = x+delta*xP;
-      Y_o(:,i) = C(2:3,:)*X_hat;
+      X = X+delta*xP;
       
-      x_hatp =A*x_hat+B*u(i)+Ko*(y(:,i)-Y_o(:,i));
-      x_hat = x_hat + delta*x_hatp;
-      
-      int = psi(i);
+      ia(i) = X(1);
+      wr(i) = X(2);
+      o(i) = X(3);
       X = [ia(i) wr(i) o(i)]';
+
+end
+x_hat=[0;0;0]
+% figure
+% hold on
+% plot(t_s, o);
+% plot(t_s, o_ref, 'Color', 'g')
+% title('Salida del sistema \theta_r(t)')
+% xlabel('Tiempo [s]')
+% ylabel('Ángulo [rad]')
+% grid
+% hold off
+X = [0;0;0]
+
+zeta = 0;
+y = 0;
+y_o = 0;
+for i = 1:(ts/delta)    
+      y = C*X;
+      zeta_p = o_ref(i)-C(2,:)*X;
+      zeta = zeta + zeta_p*delta;
+      u(i) = -K*[x_hat;zeta];
+      
+      x1_p = -Ra*X(1)/Laa-Km*X(2)/Laa+u(i)/Laa;
+      x2_p = Ki*X(1)/J-Bm*X(2)/J-T_L/J;
+      x3_p = X(2);
+      xP = [x1_p x2_p x3_p]';
+      X = X+delta*xP;
+      
+      y_o = C*x_hat;
+      x_hatp = A*x_hat+B*u(i)+Ko*(y-y_o);
+      x_hat = x_hat + delta*x_hatp;
       
       ia_o(i) = x_hat(1);
       wr_o(i) = x_hat(2);
       o_o(i) = x_hat(3);
-      X_hat = [ia_o(i) wr_o(i) o_o(i)]';
-%     psi_p = o_ref(i)-C(3,:)*X;
-%     psi(i) = int + psi_p*delta;
-%     u(i) = -K(1:3)*X-K(4)*psi(i);
-%     ia(i) = x(1);
-%     wr(i) = x(2);
-%     o(i) = x(3);
-%     x = x+delta*xP;
-%     
-%     ia_hat(i)= xObs(1);
-%     omega_hat(i)= xObs(2);
-%     theta_hat(i)= xObs(3);
-%     X = [ia(i) wr(i) o(i)]';
-%     y(i) = 
-%     y_hat(i) = C*x_hat
-%     
-%     X_hat = A*x_hat+B*u(i)+Ko*(y(:,i)-y_hat(:,i));
-%     x_hat = x_hat + X_hat*h
-%     int = psi(i)  ;
+      e_o(:,i) = y-y_o;
+
 end
 
-ia_o(i+1)=ia_o(i);
-o_o(i+1)=o_o(i);
 figure
 subplot(3,1,1)
 hold on
-plot(t_s,o,'LineWidth',1.5);
-plot(t_s, o_ref,'LineWidth',1.5, 'Color', 'g')
-plot(t_s, o_o,'LineWidth',1.5, 'Color', 'y')
+plot(t_s, o);
+plot(t_s, o_ref, 'Color', 'g')
+plot(t_s, o_o, 'Color', 'y')
 title('Salida del sistema \theta_r(t)')
 xlabel('Tiempo [s]')
 ylabel('Ángulo [rad]')
 grid
 hold off
+
 
 subplot(3,1,2)
 plot(t_s,u,'LineWidth',1.5);
@@ -161,9 +174,14 @@ grid
 subplot(3,1,3)
 hold on
 plot(t_s,ia,'LineWidth',1.5);
-plot(t_s, ia_o, 'Color', 'b')
 title('Salida del sistema i_a(t)')
 xlabel('Tiempo [s]')
 ylabel('Corriente [A]')
 grid
 hold off
+
+figure
+hold on
+plot(t_s, e_o(1,:))
+plot(t_s, e_o(2,:), 'Color', 'g')
+title('Error en la observacion')
--- Práctico 2 - Diseño de controladores en variables de estado en tiempo continuo/plan2.m
+++ Práctico 2 - Diseño de controladores en variables de estado en tiempo continuo/plan2.m
@@ -1,212 +1,501 @@
+%% Item 2 
+%% Enunciado
+% *Para el caso del avión, emplear un tiempo de integración por Euler adecuado 
+% y un tiempo de*
+% 
+% *simulación de 70seg. Los parámetros son *$a=0.07; \omega=9; b=5; c=150$* 
+% hallar un controlador para que*
+% 
+% *los polos de lazo cerrado se ubican en *$u_i=-15\pm15j; -0.5\pm0.5j$*, 
+% para referencias de 100 y -100 metros en altura, ambas con alturas iniciales 
+% de -500 y 500.*
+% 
+% * Proponer un controlador en tiempo discreto en variables de estado para que 
+% el proceso evolucione en los rangos de validez del modelo, es decir donde los 
+% ángulos y el valor de la acción de control en valor absoluto son menores a la 
+% unidad.
+% * Establecer el valor del tiempo de muestreo más adecuado para implementar 
+% el diseño en un sistema micro controlado.
+% * Determinar el efecto de la nolinealidad en la acción de control, descripta 
+% en la Fig. 4, y verificar cuál es el máximo valor admisible de la nolinealidad.
+% * Asumiendo que no puede medirse el ángulo $\alpha$, pero sí el ángulo $\phi$y 
+% la altura, proponer un esquema que permita lograr el objetivo de control.
+% 
+% 
+%% Resolución
+% Se plantea el modelo en variables de estado del avión a partir de las constantes 
+% propuestas.
+%%
 clear all; close all; clc
-%x1=alpha x2=phi x3=phi_p x4=h
-a=0.07; w=9 ;b=5; c=150;
-A = [-a  a 0 0; 0 0 1 0; w^2 -w^2 0 0; c 0 0 0];
-B = [0 0 b*w^2 0]';
-C = [0 0 0 1; 0 1 0 0];
+a = 0.07;
+b = 5;
+c = 150;    % velocidad de vuelo [m/s]
+w = 9;  
 
+Ac = [-a    a   0 0;    % alpha
+       0    0   1 0;    % phi
+      w^2 -w^2  0 0;    % phi_p
+        c   0   0 0];   % h
+Bc = [0 0 b*w^2 0]';
+
+Cc = [0 0 0 1;
+      0 1 0 0];             %salida altura y phi
+Dc = 0;
+%% 
+% A partir del analisis de la matriz controlabilidad, se determina si el 
+% sistema es controlable para $n = 4$ variables de estado.
+
+Mc = [Bc Ac*Bc Ac^2*Bc Ac^3*Bc]
+rank(Mc)
+%% 
+% El sistema es controlable. Se procede a diseñar un controlador mediante 
+% ubicacion de polos. Se utiliza la ley de control lineal de tipo $u = -Kx + Gref$
+
+% Diseño del controlador
 p1 = -15+15i;
 p2 = -15-15i;
 p3 = -0.5+0.5i;
-p4 = -0.5-0.5i;
-poles = [p1 p2 p3 p4]
-M = [B A*B A^2*B A^3*B]
-rank(M) % La matriz es de rango 4. El sistema es controlable.
+p4 = -0.5-0.5i;    
+poles = [p1 p2 p3 p4];
 
-pc = poly(A)
+pc = poly(eig(Ac));
 W = [pc(4) pc(3) pc(2) 1; pc(3) pc(2) 1 0; pc(2) 1 0 0; 1 0 0 0];  
-T = M*W
+T = Mc*W;
 
-eq = poly([p1 p2 p3 p4]);
-K = (fliplr(eq(2:end)-pc(2:end))*inv(T))
+eq = poly(poles);
+K = (fliplr(eq(2:end)-pc(2:end))*inv(T));
 
-G=-inv(C(1,:)*inv(A-B*K)*B);
+G=-inv(Cc(1,:)*inv(Ac-Bc*K)*Bc);
+%% 
+% Se procede a simular el sistema. Se debe obtener el tiempo de la dinamica 
+% mas rápida para definir el tamaño de paso en la integración de Euler.
 
-
-Ao = A'
-Bo = C'
-Co = B'
-Qo = diag([100 10000 100 100000]);    
-Ro = diag([1 1]);
-
-pc_o = poly(Ao)
-
-Mo = [Bo Ao*Bo Ao^2*Bo Ao^3*Bo]
-T = Mo*W
-Ko = (fliplr(eq(2:end)-pc(2:end))*inv(T))
-
-
+% Tiempos de simulacion
+log(0.95)/real(max(eig(Ac)))
+delta = 0.01
 tsim = 70;
-h = 1e-4;
-t = 0:h:(tsim-h);
-h_ref = [100 -100]
-ref = h(1)
-ho = [-500 500]
-pasos=tsim/h;
-%condiciones iniciales
-alpha(1)=0;
-phi(1)=0;
-phi_p(1)=0;
-high(1)=ho(1);
-u(1)=0;
-uu(1)=0;
-estados=[alpha(1);
-    phi(1);
-    phi_p(1);
-    high(1)];
+N = floor(tsim/delta);
+t = 0:delta:(tsim-delta);
 
-    
-Xop=[0 0 0 0]';
+% Prealocación de arrelgos
+alpha = zeros(1,N);
+phi = zeros(1,N);
+h = zeros(1,N);
+phi_p = zeros(1,N);
+u = zeros(1,N);
 
-x=[alpha(1);phi(1);phi_p(1); high(1)];
+% Valores iniciales y referencia
+ref = 100;
+x = [0; 0 ; 0 ; 500];
 
+% Simulación
+for i=1:N
+   
+   y_out = Cc*x;
+   u(i) = -K*x+G*ref;   
 
-alpha_obs(1)=0;
-phi_obs(1)=0;
-phip_obs(1)=0;
-high_obs(1)=0;
-xobs=[alpha_obs(1);phi_obs(1);phip_obs(1); high_obs(1)];
-estados_obs=[alpha_obs(1);
-phi_obs(1);
-phip_obs(1);
-high_obs(1)];
+   x_p = Ac*x + Bc*u(i);     
+   x  = x + x_p*delta;    
+   
 
-for i=1:pasos
-    
-    u(i)= -K*estados_obs+G*ref;
-    
-    Alin=0.1;
-    if abs(u(i))<Alin
-        uu(i)=0;
-    else
-        % si le resto la alinealidad obtengo la zona muerta en el actuador
-        % y linealidad. Sino le resto es un controlador todo o nada
-        uu(i)=sign(u(i))*(abs(u(i))-Alin);
-    end
-    
-    %Variables del sistema lineal
-    alpha(i)= x(1);
-    phi(i)= x(2);
-    phi_p(i)= x(3);
-    high(i)=x(4);
-    
-    %Sistema lineal
-    xp=A*x+B*uu(i);
-    x=x+h*xp;
-    
-    %------con Observador----------------------
+   alpha(i) = x(1);
+   phi(i)   = x(2);
+   phi_p(i) = x(3);
+   h(i)     = x(4);    
   
-    alpha_obs(i)= xobs(1);
-    phi_obs(i)= xobs(2);
-    phip_obs(i)= xobs(3);
-    high_obs(i)= xobs(4);
-    
-    ysal_o = C * estados_obs;
-    ysal   = C * estados;
-    
-    e=ysal-ysal_o;
-    
-    x_antp     = A*xobs+B*uu(i)+Ko'*e;%complicacion
-    xobs       = xobs + x_antp*h;
-    
-    %-------------------------------------------
-    estados=[alpha(i);
-    phi(i);
-    phi_p(i);
-    high(i)];
-
-    estados_obs=[alpha_obs(i);
-    phi_obs(i);
-    phip_obs(i);
-    high_obs(i)];
-
+  
+   x = [alpha(i); phi(i); phi_p(i); h(i)];
 end
 
+figure(1);
 
-%_______________SIN OBS---------------------------------------------------
-%condiciones iniciales
+subplot(3,2,1); grid on; 
+plot(t, alpha);
+title('Ángulo con la horizontal, \alpha');
+ylabel('\alpha [rad]');
+xlabel('Tiempo [s]');
 
-u_so(1)=0;
-estados_so=[alpha(1);
-    phi(1);
-    phi_p(1);
-    high(1)];
+subplot(3,2,2); grid on; 
+plot(t, phi);
+title('Ángulo de cabeceo, \phi');
+ylabel('\phi [rad]');
+xlabel('Tiempo [s]');
+
+subplot(3,2,3); grid on; 
+plot(t, phi_p);
+title('Razon de cambio del ángulo de cabeceo, \phi_p');
+ylabel('\phi_p [rad/s]');
+xlabel('Tiempo [s]');
+
+subplot(3,2,4); grid on; 
+plot(t, h);
+title('Altura de vuelo, h');
+ylabel('h [m]');
+xlabel('Tiempo [s]');
+
+subplot(3,1,3); grid on; 
+plot(t, u);
+title('Acción de control, u_t');
+ylabel('u_t [V]');
+xlabel('Tiempo [s]');
+%% 
+% Se puede observar a partir del sistema simulado que las magnitudes del 
+% sistema no son menores a la unidad. Esto implica que el modelo ya no es válido 
+% para el sistema planteado.
+% 
+% Se plantea un controlador LQR discreto para cumplir con la especificación 
+% dada. A partir del sistema discreto:
+%%
+tR = log(0.95)/real(max(eig(Ac)))
+Tm = tR/10
+
+G = ss(Ac, Bc, Cc, Dc)
+Gd = c2d(G, Tm , 'zoh');
+Ad = Gd.a
+Bd = Gd.b
+Cd = Gd.c
+Dd = Gd.d
+%% 
+% Se introduce un integrador al controlador.
+
+Aa = [Ad , zeros(4,1) ; -Cd(1,:)*Ad, eye(1)];
+Ba = [Bd; -Cd(1,:)*Bd];
+Q = diag([1e7 1e9 1e7 1 .1]);
+R = 1e7;
+K = dlqr(Aa, Ba, Q, R)';
+%% 
+% A partir del controlador diseñado, se simula el sistema.
+
+% Tiempos de simulacion
+log(0.95)/real(max(eig(Ac)))
+delta = 0.01;
+tsim = 70;
+N = floor(tsim/delta);
+t = 0:delta:(tsim-delta);
+
+% Prealocación de arrelgos
+alpha = zeros(1,N);
+phi = zeros(1,N);
+h = zeros(1,N);
+phi_p = zeros(1,N);
+u = zeros(1,N);
+
+% Valores iniciales y referencia
+ref = 100;
+x = [0; 0 ; 0 ; 500];
+psita = 0;
+% Simulación
+for i=1:N
+   y_out = Cd*x;
+   
+   psita_p = ref - y_out(1);
+   psita = psita + psita_p;
+        
+   u(i) = -K'*[x;psita];   
     
+   
+   x = Ad*x + Bd*u(i);     
+   %x  = x + x_p*delta;    
+   
 
-x_so=[alpha(1);phi(1);phi_p(1); high(1)];
-
-for i=1:pasos
-    
-    u_so(i) = -K*estados_so+G*ref;
-    %Variables del sistema lineal
-    alpha_so(i)= x_so(1);
-    phi_so(i)= x_so(2);
-    phi_p_so(i)= x_so(3);
-    high_so(i)=x_so(4);
-    
-    %Sistema lineal
-    xp_so=A*x_so+B*u_so(i);
-    x_so=x_so+h*xp_so;
-    estados_so=[alpha_so(i);
-    phi_so(i);
-    phi_p_so(i);
-    high_so(i)];
+   alpha(i) = x(1);
+   phi(i)   = x(2);
+   phi_p(i) = x(3);
+   h(i)     = x(4);    
+  
+  
+   x = [alpha(i); phi(i); phi_p(i); h(i)];
 end
 
+figure(2);
+hold on
+subplot(3,2,1); grid on;
+plot(t, alpha);
+title('Ángulo con la horizontal, \alpha');
+ylabel('\alpha [rad]');
+xlabel('Tiempo [s]');
+
+subplot(3,2,2); grid on; 
+plot(t, phi);
+title('Ángulo de cabeceo, \phi');
+ylabel('\phi [rad]');
+xlabel('Tiempo [s]');
+
+subplot(3,2,3); grid on; 
+plot(t, phi_p);
+title('Razon de cambio del ángulo de cabeceo, \phi_p');
+ylabel('\phi_p [rad/s]');
+xlabel('Tiempo [s]');
+
+subplot(3,2,4); grid on; 
+plot(t, h);
+title('Altura de vuelo, h');
+ylabel('h [m]');
+xlabel('Tiempo [s]');
+
+subplot(3,1,3); grid on; 
+plot(t, u);
+title('Acción de control, u_t');
+ylabel('u_t [V]');
+xlabel('Tiempo [s]'); hold off
+
+figure(3);
+plot(alpha, phi);
+title('Plano de fases');
+xlabel('\alpha [rad]');
+ylabel('\phi [rad]');
+%% 
+% Se procede a introducir un observador con las variables observadas siendo 
+% $\phi$ y la altura $h$.
+%%
+Ao = Ad';
+Bo = Cd';
+Co = Bd';
+
+Qo = diag([1 1 1 1]);
+Ro = diag([1 1]);
+Ko = dlqr(Ao, Bo, Qo, Ro)'
+%% 
+% Así, con el observador planteado se simula
+
+N = floor(tsim/delta);
+t = 0:delta:(tsim-delta);
+
+% Prealocación de arrelgos
+alpha = zeros(1,N);
+phi = zeros(1,N);
+h = zeros(1,N);
+phi_p = zeros(1,N);
+u = zeros(1,N);
+e_o = zeros(2,N);
+% Valores iniciales y referencia
+ref = 100;
+x = [0; 0 ; 0 ; 500];
+x_hat = [0; 0; 0; 0];
+psita = 0;
+
+% Simulación
+for i=1:N
+   y_obs = Cd*x_hat;
+   y_out = Cd*x;
+   
+   psita_p = ref - y_out(1);
+   psita = psita + psita_p;
+        
+   u(i) = -K'*[x;psita];   
     
-%----------------------------------------------------------------------
+   e_o(:,i) = y_out - y_obs;
+   x = Ad*x + Bd*u(i);      
+   x_hat = Ad*x_hat + Bd*u(i) +  Ko*(y_out - y_obs);
 
-subplot(3, 2, 1);
-hold on
-plot(t,alpha);
-plot(t,alpha_so);
-hold off
-title('Angulo con la horizontal \alpha');
-legend({'Con observador','Sin observador'})
-xlabel('Tiempo (seg.)');
-ylabel('rad');
-grid on;
+   alpha(i) = x(1);
+   phi(i)   = x(2);
+   phi_p(i) = x(3);
+   h(i)     = x(4);    
+  
+  
+   x = [alpha(i); phi(i); phi_p(i); h(i)];
+end
 
-subplot(3, 2, 2);
+figure(4);
 hold on
-plot(t,phi);
-plot(t,phi_so);
-hold off
-title('Angulo de cabeceo \phi');
-legend({'Con observador','Sin observador'})
-xlabel('Tiempo (seg.)');
-ylabel('Velocidad (m/s)');
-grid on;
+subplot(3,2,1); grid on;
+plot(t, alpha);
+title('Ángulo con la horizontal, \alpha');
+ylabel('\alpha [rad]');
+xlabel('Tiempo [s]');
 
-subplot(3, 2, 3);
-hold on
-plot(t,phi_p);
-plot(t,phi_p_so);
-hold off
-title('velocidad de angulo de cabeceo \phi_p');
-legend({'Con observador','Sin observador'})
-xlabel('Tiempo (seg.)');
-ylabel('Posicion angular (Rad/s)');
-grid on;
+subplot(3,2,2); grid on; 
+plot(t, phi);
+title('Ángulo de cabeceo, \phi');
+ylabel('\phi [rad]');
+xlabel('Tiempo [s]');
 
-subplot(3, 2, 4);
-hold on
-plot(t,high);
-plot(t,high_so);
-hold off
-title('Altura h');
-legend({'Con observador','Sin observador'})
-xlabel('Tiempo (seg.)');
-ylabel('metros');
-grid on;
+subplot(3,2,3); grid on; 
+plot(t, phi_p);
+title('Razon de cambio del ángulo de cabeceo, \phi_p');
+ylabel('\phi_p [rad/s]');
+xlabel('Tiempo [s]');
 
-subplot(3, 1, 3);
+subplot(3,2,4); grid on; 
+plot(t, h);
+title('Altura de vuelo, h');
+ylabel('h [m]');
+xlabel('Tiempo [s]');
+
+subplot(3,1,3); grid on; 
+plot(t, u);
+title('Acción de control, u_t');
+ylabel('u_t [V]');
+xlabel('Tiempo [s]'); 
+figure(5)
+plot(t, e_o);
+title('Error en la observación, h');
+xlabel('Tiempo [s]');
+xlim([0.000 0.141])
+ylim([-500 500])
+%% 
+% A continuación, se incorpora en la simulacion anterior el efecto de la 
+% zona muerta del actuador.
+%%
+N = floor(tsim/delta);
+t = 0:delta:(tsim-delta);
+
+% Prealocación de arrelgos
+alpha = zeros(1,N);
+phi = zeros(1,N);
+h = zeros(1,N);
+phi_p = zeros(1,N);
+u = zeros(1,N);
+e_o = zeros(2,N);
+% Valores iniciales y referencia
+ref = 100;
+x = [0; 0 ; 0 ; 500];
+x_hat = [0; 0; 0; 0];
+psita = 0;
+deathZone = 0.5
+% Simulación
+for i=1:N
+   y_obs = Cd*x_hat;
+   y_out = Cd*x;
+   
+   psita_p = ref - y_out(1);
+   psita = psita + psita_p;
+        
+   u(i) = -K'*[x;psita];   
+   
+   if(abs(u(i))<deathZone)
+       u(i) = 0;
+   else
+       u(i) = sign(u(i))*(abs(u(i)) - deathZone);
+   end
+   
+   e_o(:,i) = y_out - y_obs;
+   x = Ad*x + Bd*u(i);      
+   x_hat = Ad*x_hat + Bd*u(i) +  Ko*(y_out - y_obs);
+
+   alpha(i) = x(1);
+   phi(i)   = x(2);
+   phi_p(i) = x(3);
+   h(i)     = x(4);    
+  
+  
+   x = [alpha(i); phi(i); phi_p(i); h(i)];
+end
+
+figure(5);
 hold on
-plot(t,uu);
-plot(t,u_so);
-hold off
-title('Accion de control u_t');
-legend({'Con observador','Sin observador'})
-xlabel('Tiempo (seg.)');
-ylabel('V');
-grid on;
+subplot(3,2,1); grid on;
+plot(t, alpha);
+title('Ángulo con la horizontal, \alpha');
+ylabel('\alpha [rad]');
+xlabel('Tiempo [s]');
+
+subplot(3,2,2); grid on; 
+plot(t, phi);
+title('Ángulo de cabeceo, \phi');
+ylabel('\phi [rad]');
+xlabel('Tiempo [s]');
+
+subplot(3,2,3); grid on; 
+plot(t, phi_p);
+title('Razon de cambio del ángulo de cabeceo, \phi_p');
+ylabel('\phi_p [rad/s]');
+xlabel('Tiempo [s]');
+
+subplot(3,2,4); grid on; 
+plot(t, h);
+title('Altura de vuelo, h');
+ylabel('h [m]');
+xlabel('Tiempo [s]');
+
+subplot(3,1,3); grid on; 
+plot(t, u);
+title('Acción de control, u_t');
+ylabel('u_t [V]');
+xlabel('Tiempo [s]'); 
+%% 
+% Como se puede observar, la introduccion de una zona muerta en el actuador 
+% de 0.5 produce oscilaciones en la respuesta del sistema. Esto es debido a que 
+% la acción de control tiene una magnitud menor a la zona muerta. Estas oscilaciones 
+% se pueden corregir subiendo la magnitud de la acción de control para contrarrestar 
+% el efecto de la alinealidad del actuador, o buscando un actuador con menor zona 
+% muerta.
+
+N = floor(tsim/delta);
+t = 0:delta:(tsim-delta);
+
+% Prealocación de arrelgos
+alpha = zeros(1,N);
+phi = zeros(1,N);
+h = zeros(1,N);
+phi_p = zeros(1,N);
+u = zeros(1,N);
+e_o = zeros(2,N);
+% Valores iniciales y referencia
+ref = 100;
+x = [0; 0 ; 0 ; 500];
+x_hat = [0; 0; 0; 0];
+psita = 0;
+deathZone = 0.001 %Bajamos la zona muerta
+% Simulación
+for i=1:N
+   y_obs = Cd*x_hat;
+   y_out = Cd*x;
+   
+   psita_p = ref - y_out(1);
+   psita = psita + psita_p;
+        
+   u(i) = -K'*[x;psita];   
+   
+   if(abs(u(i))<deathZone)
+       u(i) = 0;
+   else
+       u(i) = sign(u(i))*(abs(u(i)) - deathZone);
+   end
+   
+   e_o(:,i) = y_out - y_obs;
+   x = Ad*x + Bd*u(i);      
+   x_hat = Ad*x_hat + Bd*u(i) +  Ko*(y_out - y_obs);
+
+   alpha(i) = x(1);
+   phi(i)   = x(2);
+   phi_p(i) = x(3);
+   h(i)     = x(4);    
+  
+  
+   x = [alpha(i); phi(i); phi_p(i); h(i)];
+end
+
+figure(6);
+hold on
+
+subplot(3,2,1); grid on;
+plot(t, alpha);
+title('Ángulo con la horizontal, \alpha');
+ylabel('\alpha [rad]');
+xlabel('Tiempo [s]');
+
+subplot(3,2,2); grid on; 
+plot(t, phi);
+title('Ángulo de cabeceo, \phi');
+ylabel('\phi [rad]');
+xlabel('Tiempo [s]');
+
+subplot(3,2,3); grid on; 
+plot(t, phi_p);
+title('Razon de cambio del ángulo de cabeceo, \phi_p');
+ylabel('\phi_p [rad/s]');
+xlabel('Tiempo [s]');
+
+subplot(3,2,4); grid on; 
+plot(t, h);
+title('Altura de vuelo, h');
+ylabel('h [m]');
+xlabel('Tiempo [s]');
+
+subplot(3,1,3); grid on; 
+plot(t, u);
+title('Acción de control, u_t');
+ylabel('u_t [V]');
+xlabel('Tiempo [s]');
